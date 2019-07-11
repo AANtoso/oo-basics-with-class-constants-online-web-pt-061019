83576ebc4fc07f3cf38051584fc2d190d2ee399e
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-11 14:40:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRANDS = ["Uggs", "Rainbow"]
+BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.any? { |b| b == brand }
   end
 
   def cobble
@@ -13,8 +14,4 @@ BRANDS = ["Uggs", "Rainbow"]
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    
-  end
 end
